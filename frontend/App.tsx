84627cbf546e262784cb54698b4e195bdc6e4bdf
Andre Vitorio <andre@vitorio.net>
2020-07-18 21:52:43 +0200

    Downgraded react-native-paper due to web version conflicts

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-native-paper';
+import { Platform } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './src/App';
 import { theme } from './src/core/theme';
@@ -11,11 +12,19 @@ const Main = () => {
   });
 
   return (
-    <ApolloProvider client={client}>
-      <Provider theme={theme}>
+    <PaperProvider theme={theme}>
+      <ApolloProvider client={client}>
+        {Platform.OS === 'web' ? (
+          <style type="text/css">{`
+        @font-face {
+          font-family: 'MaterialCommunityIcons';
+          src: url(${require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf')}) format('truetype');
+        }
+      `}</style>
+        ) : null}
         <App />
-      </Provider>
-    </ApolloProvider>
+      </ApolloProvider>
+    </PaperProvider>
   );
 };
 
