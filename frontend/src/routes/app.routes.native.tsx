1eafa2845efda4f5ddf589494c87b2ffb042ed14
Andre Vitorio <andre@vitorio.net>
2020-09-20 19:27:59 +0200

    Created alternative routes for native/web

---
+++ frontend/src/routes/app.routes.native.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Dashboard from '../screens/Dashboard';
+import Profile from '../screens/Profile';
+
+const App = createStackNavigator();
+
+const AppRoute: React.FC = () => {
+  return (
+    <App.Navigator headerMode="none">
+      <App.Screen name="Profile" component={Profile} />
+      <App.Screen name="Dashboard" component={Dashboard} />
+    </App.Navigator>
+  );
+};
+
+export default AppRoute;
