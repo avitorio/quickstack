c0af5c05fe9c6f8d14cb04e1a22765f090265ab1
Andre Vitorio <andre@vitorio.net>
2020-11-28 20:54:18 -0300

    Refactored GraphQL client.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,57 +1,29 @@
 import React from 'react';
 import { Platform, StatusBar } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-  ApolloProvider,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 import type {} from 'styled-components/cssprop';
 
 import App from './src/App';
 import { theme } from './src/core/theme';
 
 import ContextProvider from './src/context/state';
-import AsyncStorage from '@react-native-community/async-storage';
+import ApolloProvider from './src/graphql/client';
 
 const Main = () => {
-  const httpLink = createHttpLink({
-    uri: 'http://localhost:3000/graphql',
-  });
-
-  const authLink = setContext(async (_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = await AsyncStorage.getItem('@Quickstack:token');
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-
   return (
     <PaperProvider theme={theme}>
       {Platform.OS === 'ios' && (
         <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'} />
       )}
       <ContextProvider>
-        <ApolloProvider client={client}>
+        <ApolloProvider>
           {Platform.OS === 'web' ? (
             <style type="text/css">{`
-        @font-face {
-          font-family: 'MaterialCommunityIcons';
-          src: url(${require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf')}) format('truetype');
-        }
-      `}</style>
+              @font-face {
+                font-family: 'MaterialCommunityIcons';
+                src: url(${require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf')}) format('truetype');
+              }
+            `}</style>
           ) : null}
           <App />
         </ApolloProvider>
---
+++ frontend/src/graphql/client.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  ApolloProvider as ApolloProviderOriginal,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import AsyncStorage from '@react-native-community/async-storage';
+
+const ApolloProvider: React.FC = ({ children }) => {
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:3000/graphql',
+  });
+
+  const authLink = setContext(async (_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = await AsyncStorage.getItem('@Quickstack:token');
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProviderOriginal client={client}>{children}</ApolloProviderOriginal>
+  );
+};
+
+export default ApolloProvider;
