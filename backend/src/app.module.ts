fa1cdb4018a96e7cdb1eb3a1daf0e59c82a6d0fa
Andre Vitorio <andre@vitorio.net>
2020-11-29 23:30:24 -0300

    Updated default.yml and app.module to work with cors

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,18 +2,26 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
+import { MailerModule } from '@nestjs-modules/mailer';
+import * as config from 'config';
+
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { MailerModule } from '@nestjs-modules/mailer';
 import { MailerConfig } from './config/mailer.config';
-import { GraphQLError, GraphQLFormattedError } from 'graphql';
 import beautifyError from './utils/beautifyError';
+
+const serverConfig = config.get('server');
 
 @Module({
   imports: [
     MailerModule.forRoot(MailerConfig),
     GraphQLModule.forRoot({
+      cors: {
+        origin: serverConfig.origin,
+        credentials: true,
+      },
       autoSchemaFile: true,
       context: ({ req }) => ({ req }),
       debug: false,
